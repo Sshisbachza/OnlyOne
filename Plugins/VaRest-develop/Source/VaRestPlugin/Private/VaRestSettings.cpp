10bc9c45d25d4f2fb67afdd6e44cb49f424994ba
sshisbachza <psleocr@gmail.com>
2019-04-17 19:24:55 -0300

    eee

---
+++ Plugins/VaRest-develop/Source/VaRestPlugin/Private/VaRestSettings.cpp
@@ -0,0 +1,8 @@
+// Copyright 2018 Vladimir Alyamkin. All Rights Reserved.
+
+#include "VaRestSettings.h"
+
+UVaRestSettings::UVaRestSettings(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
